e23f2ac64859371bcf4d77dfc41b824a66c4e16b
Shynu Mary Varghese <Svarghese2938@conestogac.on.ca>
2021-06-03 20:36:33 +0530

    setTimerout

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const app = Vue.createApp({
   watch: {
     counter(value) {
       if (value > 50) {
-        this.counter = 0;
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
       }
     },
     //whenever name changes
